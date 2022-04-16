9d64b13f474ae5cbe6a74998d827503d42821dbf
Petro <petr@fnode>
2022-04-16 23:06:26 +0300

    hedears fix

--- src/uaheaders.cpp
+++ src/uaheaders.cpp
@@ -15,6 +15,7 @@ UaHeaders::UaHeaders()
     ff = curl_slist_append(ff, "Sec-Fetch-Site: none");
     ff = curl_slist_append(ff, "Sec-Fetch-Dest: document");
     ff = curl_slist_append(ff, "Sec-Fetch-User: ?1");
+    headers["ff99"] = ff;
 
     auto chrome = headers["chrome"];
     chrome = curl_slist_append(chrome,"accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
@@ -29,6 +30,7 @@ UaHeaders::UaHeaders()
     chrome = curl_slist_append(chrome,"sec-fetch-user: ?1");
     chrome = curl_slist_append(chrome,"upgrade-insecure-requests: 1");
     chrome = curl_slist_append(chrome,"user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36");
+    headers["chrome"] = chrome;
 
 }
 
